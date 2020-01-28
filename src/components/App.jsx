feaba11b6344dbcfc3471b7593fedc52365a4cff
SiamKing <thesiameseelectric@gmail.com>
2020-01-28 09:35:26 -0700

    Created Home component and removed unused files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Home from './Home'
+
+function App() {
+  return (
+    <div className="App">
+      Hi
+    </div>
+  );
+}
+
+export default App;
