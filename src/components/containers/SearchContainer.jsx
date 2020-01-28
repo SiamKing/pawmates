2fca489579476ba47bf3069584cbfa8540f3d397
SiamKing <thesiameseelectric@gmail.com>
2020-01-28 14:51:46 -0700

    Created Search Container

---
+++ src/components/containers/SearchContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SearchContainer extends Component {
+  render() {
+    return <div className="hero"></div>;
+  }
+}
+
+export default SearchContainer;
