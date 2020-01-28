553cf8d12d71b41a96255c65bf75ff37af81ffc3
SiamKing <thesiameseelectric@gmail.com>
2020-01-28 14:51:33 -0700

    Added header to hero with styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,9 @@ class Home extends Component {
     render() {
         return (
             <div className="hero">
+                <div className="hero-text">
+                    <h1 className="display-4">Find your <span className="font-weight-normal">Pawmate</span> today</h1>
+                </div>
             </div>
         )
     }
