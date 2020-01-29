754b87c8650c39ce0c7ae306f0d9c46f827d8aa3
SiamKing <thesiameseelectric@gmail.com>
2020-01-29 16:27:58 -0700

    Added cases for adding animals from fetch in reducer

--- src/reducers/manageAnimals.jsx
+++ src/reducers/manageAnimals.jsx
@@ -1,7 +1,19 @@
-export default function manageAnimals(state = { animals: ["Dog"] }, action) {
+export default function manageAnimals(state = { animals: [], requesting: false }, action) {
     switch (action.type) {
-        case "ADD":
-            return state;
+        case "START_ADDIN_ANIMALS_REQUEST":
+            return {
+                ...state,
+                animals: [...state.animals],
+                requesting: true
+            }
+
+        case "ADD_ANIMALS":
+            return {
+                ...state,
+                animals: action.animals.data,
+                requesting: false
+            }
+
         default:
             return state;
     }
