ec8f583388ba9b6d9aeb0b8584eb4c51a7426cc1
SiamKing <thesiameseelectric@gmail.com>
2020-01-31 09:00:29 -0700

    Updated Search Results to hand down props to child compenent

--- src/components/containers/SearchResultsContainer.jsx
+++ src/components/containers/SearchResultsContainer.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
+import Animals from '../animals/Animals';
+
 class SearchResultsContainer extends Component {
 
     render() {
         console.log(this.props)
         return (
             <div>
-                
+                {this.props.animals ?
+                    <Animals animals={this.props.animals} />
+                    : ""
+                }}
             </div>
         )
     }
