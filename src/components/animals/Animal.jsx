17da77dcc2b8c29cc0edef847d2fb572cf406392
SiamKing <thesiameseelectric@gmail.com>
2020-02-03 12:43:46 -0700

    Updated Animal component card styling - semi-finalized

--- src/components/animals/Animal.jsx
+++ src/components/animals/Animal.jsx
@@ -6,26 +6,18 @@ import Button from 'react-bootstrap/button'
 class Animal extends Component {
     render() {
         let { animal } = this.props
-        animal = animal.attributes
         console.log(animal)
-        return (
-            <Col xs={12} sm={6} md={4} lg={3} className="my-3">
+        return ( 
+            <Col xs={12} sm={6} md={4} lg={3} className="my-3 px-sm-2 px-xs-4">
                 <Card>
-                    <div className="p-1">
-                            <Card.Img className="img-thumbnail float-right" variant="right" src={animal.pictureThumbnailUrl} />
-                        <div className="float-left pl-1">
-                            <div className="mb-0 title-card">{animal.name}</div>
-                            <p className="text-muted my-0">{animal.sex}</p>
-                            <p className="text-muted my-0">{animal.newPeopleReaction}</p>
-                            <p className="text-muted my-0">{animal.qualities[0]}</p>
-                        </div>
-
-                    </div>
+                    <Card.Img className="img-thumbnail" variant="top" src={animal.picture} />
                     <Card.Body>
-                        <Card.Text>
+                        <Card.Title>{animal.name} - <span className="text-muted small">{animal.sex}</span></Card.Title>
+                        <Card.Subtitle className="text-muted">{animal.breedString}</Card.Subtitle>
+                        <Card.Text className="mt-3">
                             <div dangerouslySetInnerHTML={{ __html: animal.descriptionText }} className="truncate-text" ></div>
                         </Card.Text>
-                        <Button variant="primary" href={animal.url} target="_blank">Learn More</Button>
+                        <Button variant="primary" href={animal.url} target="_blank" className="align-self-end">Learn More</Button>
                     </Card.Body>
                 </Card>
             </Col>
