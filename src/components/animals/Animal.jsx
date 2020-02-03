adcbe8c4222c3ea6e233e2e5dd6d4db8cadecd31
SiamKing <thesiameseelectric@gmail.com>
2020-02-03 13:23:01 -0700

    Fixed error with card-text nested p tag

--- src/components/animals/Animal.jsx
+++ src/components/animals/Animal.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/button'
+import Button from 'react-bootstrap/Button'
 
 class Animal extends Component {
     render() {
         let { animal } = this.props
-        console.log(animal)
-        return ( 
+        return (
             <Col xs={12} sm={6} md={4} lg={3} className="my-3 px-sm-2 px-xs-4">
                 <Card>
                     <Card.Img className="img-thumbnail" variant="top" src={animal.picture} />
                     <Card.Body>
                         <Card.Title>{animal.name} - <span className="text-muted small">{animal.sex}</span></Card.Title>
                         <Card.Subtitle className="text-muted">{animal.breedString}</Card.Subtitle>
-                        <Card.Text className="mt-3">
-                            <div dangerouslySetInnerHTML={{ __html: animal.descriptionText }} className="truncate-text" ></div>
-                        </Card.Text>
+                        <div className="mt-3 card-text">
+                            <p dangerouslySetInnerHTML={{ __html: animal.descriptionText }} className="truncate-text"></p>
+                        </div>
                         <Button variant="primary" href={animal.url} target="_blank" className="align-self-end">Learn More</Button>
                     </Card.Body>
                 </Card>
