1ee6a969d96549d86fd4099eebead588c9d2d0ce
SiamKing <thesiameseelectric@gmail.com>
2020-02-03 12:45:52 -0700

    Added fetch/dispatch to search component

--- src/components/containers/SearchContainer.jsx
+++ src/components/containers/SearchContainer.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 import Search from '../search/Search'
+import { fetchAnimals } from '../../actions/fetchAnimals'
 
 class SearchContainer extends Component {
+
+    handleDispatch = searchFields => {
+        this.props.fetchAnimals(searchFields)
+    }
     render() {
         return (
             <div className="search-container mt-lg-4 p-3">
-                <Search />
+                <button onClick={this.handleDispatch}>Click</button>
+                {/* <Search handleDispatch={this.handleDispatch} /> */}
             </div>
         )
     }
 }
 
-export default SearchContainer;
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchAnimals: (searchFields) => dispatch(fetchAnimals(searchFields))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SearchContainer);
