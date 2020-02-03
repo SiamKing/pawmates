7af94a84d038e315d27b016ca9aa17cc33d6ff69
SiamKing <thesiameseelectric@gmail.com>
2020-02-03 12:44:36 -0700

    Add margin top and bottom to rows for animal component

--- src/components/animals/Animals.jsx
+++ src/components/animals/Animals.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-
+import CardGroup from 'react-bootstrap/CardGroup'
 import Animal from './Animal'
+import { Card } from 'react-bootstrap'
 
 class Animals extends Component {
 
     render() {
         const { animals } = this.props
+        console.log(animals)
         return (
             <Container>
-                <Row>
+                <Row className="my-2">
+                    <CardGroup>
                     {animals.map(animal => {
                         return <Animal animal={animal} />
                     })}
+                    </CardGroup>
                 </Row>
             </Container>
         )
