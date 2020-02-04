3abbcfa08e12c7ddec5f04e5eb58999f4bf47b80
SiamKing <thesiameseelectric@gmail.com>
2020-02-03 19:58:23 -0700

    Added hard coded searchFields for api request

--- src/components/containers/SearchContainer.jsx
+++ src/components/containers/SearchContainer.jsx
@@ -7,12 +7,16 @@ import { fetchAnimals } from '../../actions/fetchAnimals'
 class SearchContainer extends Component {
 
     handleDispatch = searchFields => {
-        this.props.fetchAnimals(searchFields)
+        this.props.fetchAnimals({
+          species: "cats",
+          zipCode: 85281,
+          radius: 25
+        });
     }
     render() {
         return (
             <div className="search-container mt-lg-4 p-3">
-                <button onClick={this.handleDispatch}>Click</button>
+                <button onClick={() => this.handleDispatch({species: 'cats', zipCode: 85281, radius: 25})}>Click</button>
                 {/* <Search handleDispatch={this.handleDispatch} /> */}
             </div>
         )
