68e7ef36d033c9ee8f8f70ce1fb4680eba4960fe
SiamKing <thesiameseelectric@gmail.com>
2020-02-05 11:40:57 -0700

    Added routes and wildcard route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Home from './Home'
 import Navbar from './Navbar'
 import SearchResultsContainer from './containers/SearchResultsContainer';
 
-function App() {
+const Wildcard = () => {
+  return <Redirect to="/" />
+}
+
+const App = ({ history, context }) => {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <SearchResultsContainer />
-    </div>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/pawmates" component={SearchResultsContainer} />
+            <Route exact path="/*" component={Wildcard} />
+          </Switch>
+        </>
   );
 }
 
