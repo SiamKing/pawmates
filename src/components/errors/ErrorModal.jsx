57bd61e1000626bd7651bfeabc7b8d45a6f1d2fc
SiamKing <thesiameseelectric@gmail.com>
2020-02-06 09:49:09 -0700

    Added ERRORS const to hold array of possible errors with titles and descriptions for modal

--- src/components/errors/ErrorModal.jsx
+++ src/components/errors/ErrorModal.jsx
@@ -2,18 +2,33 @@ import React, { Component } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 
+
+const ERRORS = [
+    {
+        title: "Sorry, no pawmates were found in your search.",
+        description: "Please change your search criteria (maybe try expanding the distance)."
+    },
+    {
+        title: "Whoops! Something went wrong!",
+        description: "Please try your search again."
+    }
+]
+
 class ErrorModal extends Component {
 
     render() {
+        const { showModal, closeModal, error } = this.props
+        const ERROR = ERRORS[error]
+        const { title, description } = ERROR
         return (
             <>
-                <Modal centered show={this.props.showModal} onHide={this.props.closeModal}>
+                <Modal centered show={showModal} onHide={closeModal}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
+                        <Modal.Title>{title}</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Body>{description}</Modal.Body>
                     <Modal.Footer>
-                        <Button variant="primary" onClick={this.props.closeModal}>
+                        <Button variant="primary" onClick={closeModal}>
                             Close
                     </Button>
                     </Modal.Footer>
