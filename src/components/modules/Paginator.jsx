20e7280ebd01f51f8ff1eac36f50c65dcb2648c0
SiamKing <thesiameseelectric@gmail.com>
2020-02-06 15:58:49 -0700

    Updated Pagintor with functionality to handle small or large numbers of pages

---
+++ src/components/modules/Paginator.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Pagination from 'react-bootstrap/Pagination'
+import Row from 'react-bootstrap/Row'
+
+const Paginator = ({ meta, handleDispatch }) => {
+
+    const { count, countReturned, pageReturned, limit, pages } = meta;
+    const beginAnimals = (pageReturned - 1) * 24 + 1;
+    const lastAnimals = pageReturned === pages ? count : beginAnimals + 23;
+
+    const range = (start, end) => {
+        let pageLimit = pages < 4 ? pages - 1 : 4;
+        let pageStart = end > pages ? pages - pageLimit : start;
+        let pageEnd = end > pages ? pages : end;
+        return Array(pageEnd - pageStart + 1).fill().map((_, i) => pageStart + i)
+    }
+    return (
+        <>
+            <Row className="justify-content-md-center mt-4">
+                <Pagination>
+                    <Pagination.First onClick={() => handleDispatch(pages[0])} />
+                    {
+                        pageReturned === 1 ? null :
+                            <Pagination.Prev onClick={() => handleDispatch(pageReturned - 1)} />
+                    }
+                    {/* <Pagination.Item active={pageReturned == 1}>{1}</Pagination.Item> */}
+
+                    { pages ?
+                        range(pageReturned, pageReturned + 4).map((pageNumber, i) => {
+                            return (
+                                <Pagination.Item
+                                    key={pageNumber}
+                                    active={pageReturned === pageNumber}
+                                    onClick={() => handleDispatch(pageNumber)}
+                                >{pageNumber}
+                                </Pagination.Item>
+                            )
+                        })
+                        : ''
+                    }
+
+                    {/* <Pagination.Item >{pages}</Pagination.Item> */}
+                    { pageReturned === pages ? null :
+                        <Pagination.Next onClick={() => handleDispatch(pageReturned + 1)} />
+                    }
+                    <Pagination.Last onClick={() => handleDispatch(pages)} />
+                </Pagination>
+            </Row>
+            <Row className="justify-content-md-center">
+                <p>Showing {beginAnimals} thru {lastAnimals} of {count}  </p>
+            </Row>
+        </>
+    )
+}
+
+export default Paginator;
