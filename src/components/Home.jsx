22b2f1c500074833d37b9d2b523c7c34da7eb719
SiamKing <thesiameseelectric@gmail.com>
2020-02-06 09:07:15 -0700

    Imported ErrorModal and recieved new state property from redux for showing modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 
 import SearchContainer from './containers/SearchContainer'
 import Loader from "./Loader";
+import ErrorModal from './errors/ErrorModal'
 
 
 class Home extends Component {
@@ -13,13 +14,19 @@ class Home extends Component {
         if (this.props.animals.length > 0) {
             this.props.goToPawmatesPage();
         }
+
+    }
+
+    handleCloseModal = () => {
+        this.props.closeErrorModal();
     }
 
     render() {
-        console.log(this.props)
+        console.log(this.props.error)
         return (
             <div className="hero">
                 <Loader loading={this.props.loading} />
+                <ErrorModal showModal={this.props.showErrorModal} closeModal={this.handleCloseModal} />
                 <div className="hero-container d-flex flex-column">
                     <div className="hero-text-wrapper">
                         <div className="hero-text">
@@ -39,12 +46,17 @@ class Home extends Component {
 const mapStateToProps = state => {
     return {
         animals: state.animals.animals,
-        loading: state.animals.requesting
+        loading: state.animals.requesting,
+        error: state.animals.error,
+        showErrorModal: state.animals.showErrorModal
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    goToPawmatesPage: () => push('/pawmates')
-}, dispatch)
+const mapDispatchToProps = dispatch => {
+    return {
+        goToPawmatesPage: () => dispatch(push('/pawmates')),
+        closeErrorModal: () => dispatch({ type: "CLOSE_MODAL" })
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
