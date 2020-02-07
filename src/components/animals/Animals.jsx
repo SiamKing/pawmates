7e62f3251dc91f1cad3fa5c6fd002b2f3eff7084
SiamKing <thesiameseelectric@gmail.com>
2020-02-07 09:19:25 -0700

    Added jumbotron to animals page as well as dynamic css to change image based on species

--- src/components/animals/Animals.jsx
+++ src/components/animals/Animals.jsx
@@ -2,26 +2,34 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import CardGroup from 'react-bootstrap/CardGroup'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 import Animal from './Animal'
 import Paginator from '../modules/Paginator'
 class Animals extends Component {
 
     render() {
-        const { animals, meta, handleDispatch } = this.props;
+        const { animals, meta, handleDispatch, searchFields } = this.props;
 
         return (
-            <Container>
-                <Row className="my-2">
-                    <CardGroup>
-                        {animals.map(animal => {
-                            return <Animal key={animal.id} animal={animal} />
-                        })}
-                    </CardGroup>
-                </Row>
+            <>
+                <Jumbotron fluid className={`results-hero-${searchFields.species}`}>
+                    <div className="ml-5 mt-5">
+                        <h1 className="ml-5 display-4">{searchFields.species}</h1>
+                    </div>
+                </Jumbotron>
+                <Container>
+                    <Row className="my-2">
+                        <CardGroup>
+                            {animals.map(animal => {
+                                return <Animal key={animal.id} animal={animal} />
+                            })}
+                        </CardGroup>
+                    </Row>
 
-                <Paginator meta={meta} handleDispatch={handleDispatch} />
-            </Container>
+                    <Paginator meta={meta} handleDispatch={handleDispatch} />
+                </Container>
+            </>
         )
     }
 }
