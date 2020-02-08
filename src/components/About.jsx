6af23be37818de56dd76cc0f08bb66f8728e7451
SiamKing <thesiameseelectric@gmail.com>
2020-02-08 16:49:11 -0700

    Added About page

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about-wrapper">
+            <div className="about-hero d-none d-md-block"></div>
+            <div className="about-hero-container d-flex flex-column">
+                <div className="hero-text-wrapper">
+                    <div className="about-hero-text">
+                        <h4 className="font-weight-light about-description">
+                            Do you believe in soulmates? Maybe felt something has been missing your whole life? Well, maybe you're soulmate has four paws. It's our job to help you in your search for your pawmate! We'll use our giddygacks and pillypacks (patented technology) to search the known universe to find your pawmate. You just might find what you've been looking for...
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
